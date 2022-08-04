4ac8d8170407ced8189558bcc0ce59754e45196b
sasanzare <sasanzare75@gmail.com>
2022-08-04 21:03:32 +0430

    Server created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8000".to_string());
+    server.run();
+}
+struct Server{
+    addr: String,
+}
+
+impl Server{
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self){
+        println!("Listening on {}", self.addr)
+    }
 }
