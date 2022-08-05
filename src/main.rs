0d29fef1855a41e1ac361ea139b267e879eb36a9
sasanzare <sasanzare75@gmail.com>
2022-08-05 22:06:14 +0430

    add mod server and mod http

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,54 @@
+use std::option::Option;
+
+use server::Server;
+use http::request::Request;
+
+
 fn main() {
     let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
-struct Server{
-    addr: String,
+
+mod server{
+    pub struct Server{
+        addr: String,
+    }
+    
+    impl Server{
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+    
+        pub fn run(self){
+                println!("Listening on {}", self.addr);
+            }
+        }
+    
 }
 
-impl Server{
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
 
-    fn run(self){
-        println!("Listening on {}", self.addr)
-    }
+mod http {
+
+    pub mod request{
+            use super::method::Method;
+            pub struct Request {
+                    path: String,
+                    query_string: Option<String>,
+                    method: Method,
+                }
+        }
+    
+    pub mod method{
+            pub  enum Method {
+                    GET,
+                    POST,
+                    DELETE,
+                    PUT,
+                    HEAD,
+                    CONNECT,
+                    OPTIONS,
+                    TRACE,
+                    PATH,
+                }
+        }
 }
