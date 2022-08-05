5866230b46221b96d60ce299c24a266d2129db27
sasanzare <sasanzare75@gmail.com>
2022-08-05 22:29:35 +0430

    http mod created

---
+++ src/http/mod.rs
@@ -0,0 +1,6 @@
+pub use method::Method;
+pub use request::Request;
+
+
+mod method;
+pub mod request;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::option::Option;
 
 use server::Server;
-use http::request::Request;
+use http::Method;
+use http::Request;
+
 
 
 mod server;
+mod http;
 
 fn main() {
     let server = Server::new("127.0.0.1:8000".to_string());
@@ -12,29 +14,3 @@ fn main() {
 }
 
 
-
-mod http {
-
-    pub mod request{
-            use super::method::Method;
-            pub struct Request {
-                    path: String,
-                    query_string: Option<String>,
-                    method: Method,
-                }
-        }
-    
-    pub mod method{
-            pub  enum Method {
-                    GET,
-                    POST,
-                    DELETE,
-                    PUT,
-                    HEAD,
-                    CONNECT,
-                    OPTIONS,
-                    TRACE,
-                    PATH,
-                }
-        }
-}
