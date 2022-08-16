e95e4ed6d5635d34a3a86479eb40ea0e70e982d1
sasanzare <sasanzare75@gmail.com>
2022-08-16 09:34:00 +0430

    Edit life times

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,36 +5,41 @@ use std::error::Error;
 use std::fmt::{Debug,Formatter,Display, Result as FmtResult};
 use std::str;
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error>{
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequset)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequset)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequset)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequset)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
 
-        let method:: Method = method.parse()?;
+        let method: Method = method.parse()?;
 
         let mut query_string = None;
         if let Some(i) = path.find('?'){
             query_string = Some(&path[i + 1..]);
             path = &path[..i];
         }
-
-        unimplemented!()
+        
+        Ok(Self{
+            path,
+            query_string,
+            method,
+        })
+        // unimplemented!()
     }
 }
 
@@ -91,4 +96,4 @@ impl Debug for ParseError {
     }
 }
 
-impl Error for parseError {}
+// impl Error for parseError {}
