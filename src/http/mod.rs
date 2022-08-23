172b34ad0295e0971f47f5a9aba02ecba4f77787
sasanzare <sasanzare75@gmail.com>
2022-08-23 13:20:29 +0430

    Add mod response

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,8 +1,10 @@
 pub use method::Method;
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
 pub use query_string::{QueryString, Value as QueryStringValue};
 
 pub mod method;
 pub mod request;
+pub mod response;
 pub mod query_string;
